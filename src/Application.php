90bda282625d612be89bf0ddd9b9af08598e4ac2
Wake Liu <wake.gs@gmail.com>
2019-01-28 16:59:46 +0800

    Add customizable lang path

--- src/Application.php
+++ src/Application.php
@@ -7,4 +7,20 @@
 
   class Application extends Lumen\Application {
 
+
+    /**
+     * Get the path to the application's language files.
+     *
+     * @return string
+     */
+    protected function getLanguagePath () {
+
+      $langPath = env ('APP_LANG_PATH', '/resource/lang');
+
+      if (is_dir ($path = $this->basePath () . $langPath))
+        return $path;
+
+      else
+        return __DIR__ . '/..' . $langPath;
+    }
   }
